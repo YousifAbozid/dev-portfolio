28de12390b26a2455c56412bfc6b0faae2f0253d
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-04 07:57:36 +0200

    add portfolio projects component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import portfolio from './data/portfolio'
+import PortfolioItem from './PortfolioItem'
 
 const Portfolio = () => {
-  return <div>Portfolio</div>
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {portfolio.map((project, index) => (
+          <>
+            <PortfolioItem
+              key={index}
+              title={project.title}
+              imgUrl={project.imgUrl}
+              stack={project.stack}
+              link={project.link}
+            />
+          </>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Portfolio
---
+++ src/components/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+	{
+		title: 'Tvflix',
+		imgUrl: './assets/tvflix.png',
+		stack: ['HTML', 'CSS', 'JavaScript'],
+		link: 'https://github.com/YousifAbozid'
+	},
+	{
+		title: 'Project 2',
+		imgUrl: 'https://via.placeholder.com/150',
+		stack: ['React', 'Node', 'Express', 'MongoDB'],
+		link: 'https://github.com/YousifAbozid'
+	},
+	{
+		title: 'Project 3',
+		imgUrl: 'https://via.placeholder.com/150',
+		stack: ['React', 'Node', 'Express', 'MongoDB'],
+		link: 'https://github.com/YousifAbozid'
+	},
+]
