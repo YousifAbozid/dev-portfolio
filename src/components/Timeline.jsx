d29e2cfd20c649bb0a3537b5caa33fe0082f7a4a
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-08 01:55:50 +0200

    add the timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import timeline from './data/timeline'
+import TimelineItem from './TimelineItem'
 
 const Timeline = () => {
-  return <div>Timeline</div>
+  return (
+    <div className="flex flex-col md:flex-row justify-center my-20">
+      <div className="w-full md:w-7/12">
+        {timeline.map((item, index) => {
+          return (
+            <TimelineItem
+              key={index}
+              year={item.year}
+              title={item.title}
+              duration={item.duration}
+              details={item.details}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Timeline
---
+++ src/components/data/timeline.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    year: 2018,
+    title: 'Started working on the project',
+    duration: '2011',
+    details:
+      'I started working on the project in 2011, and it was a great success.',
+  },
+  {
+    year: 2018,
+    title: 'Started working on the project',
+    duration: '2011',
+    details:
+      'I started working on the project in 2011, and it was a great success.',
+  },
+  {
+    year: 2018,
+    title: 'Started working on the project',
+    duration: '2011',
+    details:
+      'I started working on the project in 2011, and it was a great success.',
+  },
+]
