024c88ea726c184521fc1386c6a4d81597789082
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-08 02:29:32 +0200

    add contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
 
 const Contact = () => {
-  return <div>Contact</div>
+  return (
+    <div className="flex flex-col mb-10 mx-auto">
+      <div className="flex justify-center items-center">
+        <form
+          action="https://getform.io/f/90201b71-e03a-49e0-a0a8-b7f3a3f6981f"
+          method="POST"
+          className="flex flex-col w-full md:w-7/12">
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            className="my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            rows="10"
+            className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
+          />
+          <button
+            type="submit"
+            className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white">
+            Work With Me
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default Contact
