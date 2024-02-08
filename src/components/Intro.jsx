37fb02a7b05ee8d1b45de23a621d12cfc69ddc3b
Yousif Abozid <yousif.abozid@yahoo.com>
2024-02-08 09:15:09 +0200

    add dark mode

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,15 +3,23 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className="flex items-center justify-center flex-col pt-20 pb-6">
-      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Yousif Abozid</h1>
-      <p className="text-base md:text-1xl mb-3 font-medium">Full Stack Software Engineer</p>
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold dark:text-white">
+        Yousif Abozid
+      </h1>
+      <p className="text-base md:text-1xl mb-3 font-medium">
+        Full Stack Software Engineer
+      </p>
       <p className="text-sm max-w-xl mb-6 font-bold">
-        I'm a self-built software engineer, sculpted by the rigorous programs of Udacity, Helsinki University, and ALX
-        Africa's Software Engineering Program. Embracing the dynamic world of coding, I find my passion in unraveling
-        complex challenges and crafting elegant solutions. Yet, my journey extends beyond the realm of technology. As a
-        novelist, I weave intricate tales, breathing life into characters and worlds. Additionally, as an entrepreneur,
-        I helm the ship of innovation with my venture, fostering a multidimensional approach to creativity. With each
-        endeavor, I embrace the thrill of exploration and the satisfaction of creation. 🚀📚💻
+        I'm a self-built software engineer, sculpted by the rigorous programs of
+        Udacity, Helsinki University, and ALX Africa's Software Engineering
+        Program. Embracing the dynamic world of coding, I find my passion in
+        unraveling complex challenges and crafting elegant solutions. Yet, my
+        journey extends beyond the realm of technology. As a novelist, I weave
+        intricate tales, breathing life into characters and worlds.
+        Additionally, as an entrepreneur, I helm the ship of innovation with my
+        venture, fostering a multidimensional approach to creativity. With each
+        endeavor, I embrace the thrill of exploration and the satisfaction of
+        creation. 🚀📚💻
       </p>
     </div>
   )
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -7,15 +7,14 @@ const Portfolio = () => {
     <div className="flex flex-col md:flex-row items-center justify-center">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {portfolio.map((project, index) => (
-          <>
+          <React.Fragment key={index}>
             <PortfolioItem
-              key={index}
               title={project.title}
               imgUrl={project.imgUrl}
               stack={project.stack}
               link={project.link}
             />
-          </>
+          </React.Fragment>
         ))}
       </div>
     </div>
